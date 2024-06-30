0ebfd4e5a3907bc9f05f58574630a098760c13e5
Kirill Liapustin <asaprokki@gmail.com>
2024-06-30 16:13:04 +0500

    Добавьте файлы проекта.

---
+++ Fraction.hpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+class Fraction {
+private:
+    unsigned int FirstNumber;
+    unsigned int SecondNumber;
+    bool positive;
+
+
+    void reduce();
+
+
+    unsigned int NOD(unsigned int a, unsigned int b);
+
+public:
+
+    Fraction(unsigned int num, unsigned int denom, bool pos);
+
+    Fraction();
+  
+    Fraction(const Fraction& other);
+
+    void print() const;
+    Fraction sum(const Fraction& other) const;
+    Fraction subtraction(const Fraction& other)const;
+
+    Fraction sum(unsigned int number) const;
+    Fraction subtraction(unsigned int number) const;
+
+
+
+
+};
---
+++ Main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include "Fraction.hpp"
+
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	Fraction f1(9, 7, true);
+	Fraction f2(4, 9, true);
+
+
+	std::cout << "Дробь f1 = ";
+	f1.print();
+	std::cout << std::endl;
+	std::cout << "Дробь f2 = ";
+	f2.print();
+	std::cout << std::endl;
+
+	Fraction sum = f1.sum(f2);
+	std::cout << "Сумма f1 и f2: ";
+	sum.print();
+	std::cout << std::endl;
+
+	Fraction diff = f1.subtraction(f2);
+	std::cout << "Разность f1 и f2: ";
+	diff.print();
+	std::cout << std::endl;
+
+
+
+
+	return 0;
+
+}
